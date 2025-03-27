bf239ec675827b3820beb3abcdc843c07f603f65
Tharanitharan Muthuthirumaran <tharani@Mac-7900.lan>
2025-03-26 21:11:29 -0400

    Aurinko callback need to do GoogleOAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import { Button } from "@/components/ui/button";
+import LinkAccountButton from "@/components/link-account-button";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <LinkAccountButton />
   );
 }
---
+++ src/components/link-account-button.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import { getAurinkoAuthUrl } from '@/lib/aurinko'
+
+const LinkAccountButton = () => {
+    return (
+        <Button onClick={async () => {
+            const url = await getAurinkoAuthUrl('Google')
+            window.location.href = url
+        }}>
+            Link Account
+        </Button>
+    )
+}
+
+export default LinkAccountButton
